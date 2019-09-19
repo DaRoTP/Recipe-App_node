07bc1c61bfc73c57a3556433f6b6276bde3cf139
DaRoTP <DRodzewicz@gmail.com>
2019-09-19 18:43:21 +0300

    setting up basic routes -sets up partials (footer, header) -set up views index, new, recipe -adds scss

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+//package dependencies
 const express = require('express');
+
+//models
 
 const app = express();
 const PORT = 3000 || env.local.PORT;
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
+
 app.get("/", (req, res) =>{
-    res.send("test");
+    res.render("index");
+});
+
+app.get("/recipes", (req, res) =>{
+    res.render("recipes/recipes");
+});
+
+app.get("/recipes/new", (req, res) =>{
+    res.render("recipes/new");
 });
 
 app.listen(PORT, () =>{
